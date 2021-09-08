9fcd6117de83e1796e1de7e9a14248bfbcdd9ee0
DanielRagusa12 <daniel.ragusa129@gmail.com>
2021-09-08 15:37:29 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Daniel Ragusa
+ */
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.print("What is the quote? ");
+        String Quote;
+        Scanner input=new Scanner(System.in).useDelimiter("\n");
+        Quote=input.next();
+
+        System.out.print("Who said it? ");
+        String Author;
+        Scanner input2=new Scanner(System.in).useDelimiter("\n");
+        Author=input2.next();
+        System.out.println((Author) + " says," + " \"" + (Quote) + "\"");
+    }
+}
